f3d53340458359f7e21668751f2a4f9e3138626d
MrKrrot <rafita.olguin@hotmail.com>
2023-05-25 16:50:30 -0500

    docs: Fix env variable name DB_URI in config file

--- src/config/index.js
+++ src/config/index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 const { DB_URI, NODE_ENV, PORT } = process.env
 
-if(!DB_URI) throw new Error('MONGO_URI env variable is not defined')
+if(!DB_URI) throw new Error('DB_URI env variable is not defined')
 if(!NODE_ENV) throw new Error('NODE_ENV env variable is not defined')
 if(!PORT) throw new Error('PORT env variable is not defined')
 
