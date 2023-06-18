ae19ba2d4ac4a20c9970e41b0bc596f8c4357657
Alejandro Avalos <alejandroavalos2401@gmail.com>
2023-06-17 23:17:45 -0500

    refactor: New design of the whitebox component (only the icons)

--- client/src/components/Whitebox/index.jsx
+++ client/src/components/Whitebox/index.jsx
@@ -1,19 +1,15 @@
 import Image from 'next/image'
 
-export const WhiteBox = ({ txt }) => {
+export const WhiteBox = ({ txt, icon }) => {
   return (
-    <div className='relative bg-white py-6 px-6 rounded-3xl w-64 my-4 shadow-xl sm:w-72'>
-      <div className=' text-white flex items-center absolute rounded-full px2 shadow-xl bg-pink-500 left-4 -top-6'>
-        <Image
-          src='/images/icon-teacher.png'
-          alt='Teacher icon'
-          width={64}
-          height={64}
-          className='h-16 w-16'
-        />
+    <div class="relative bg-[#FFF] py-6 px-6 rounded-3xl w-64 my-4 shadow-xl sm:w-72">
+      <div class="text-[#FFF] flex items-center absolute rounded-full px2 shadow-xl bg-pink-500 left-4 -top-6">
+        <div class="bg-yellow-600 p-1.5 rounded-lg">
+          {icon}
+        </div>
       </div>
-      <div className='mt-8'>
-        <p className='text-xl font-semibold my-2 text-dark_gray'>{txt}</p>
+      <div class="mt-8">
+        <p class="text-xl font-semibold my-2 text-dark_gray">{txt}</p>
         <h2 className='text-7xl font-bold text-dark_gray'>40</h2>
       </div>
     </div>
